faccf3c747ba57e3d8252e47491d9e305aa878b7
Simone Taeggi <simone.taeggi@leovegas.com>
2018-03-23 15:21:18 +0100

    Stateless Functional Components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,18 @@
 import React from 'react';
-class Header extends React.Component {
-  render() {
-    return (
-      <header className="top">
-        <h1>Pesce
-          <span className="ofThe">
-            <span className="of">Di</span>
-            <span className="the">Ogni</span>
-          </span>
-          Giorno</h1>
-        <h3 className="tagline">
-          <span>{this.props.tagline}</span>
-        </h3>
-      </header>
-    );
-  }
-}
+
+const Header = ({tagline, age}) => (
+  <header className="top">
+    <h1>Pesce
+      <span className="ofThe">
+        <span className="of">Di</span>
+        <span className="the">Ogni</span>
+      </span>
+      Giorno</h1>
+    <h3 className="tagline">
+      <span>{tagline}</span>
+    </h3>
+  </header>
+);
+
 
 export default Header;
