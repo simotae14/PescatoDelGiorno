c0758b4ab10f3e82998dd3a69bbedaeae73d231a
Simone Taeggi <simone.taeggi@leovegas.com>
2018-03-23 16:48:23 +0100

    React with React Router

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,17 @@
 import React from 'react';
 
 const Header = ({tagline, age}) => (
-  <header className="top">
-    <h1>Pesce
-      <span className="ofThe">
-        <span className="of">Di</span>
-        <span className="the">Ogni</span>
-      </span>
+	<header className="top">
+		<h1>Pesce
+		<span className="ofThe">
+			<span className="of">Di</span>
+			<span className="the">Ogni</span>
+		</span>
       Giorno</h1>
-    <h3 className="tagline">
-      <span>{tagline}</span>
-    </h3>
-  </header>
+		<h3 className="tagline">
+			<span>{tagline}</span>
+		</h3>
+	</header>
 );
 
 
