b475eb4480aa9376945b730758749c4ad46e94ac
Simone Taeggi <simone.taeggi@leovegas.com>
2018-03-23 14:37:20 +0100

    Passare dati Dinamici coi Props

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,16 @@
 import React from 'react';
-
 class Header extends React.Component {
   render() {
     return (
       <header className="top">
-        <h1>Pescato
+        <h1>Pesce
           <span className="ofThe">
             <span className="of">Di</span>
             <span className="the">Ogni</span>
           </span>
           Giorno</h1>
         <h3 className="tagline">
-          <span>Fresco ogni Giorno</span>
+          <span>{this.props.tagline}</span>
         </h3>
       </header>
     );
