cabb1a2cd571b0f79615fbdb7c17e224cbf482d2
Simone Taeggi <simone.taeggi@leovegas.com>
2018-03-28 15:51:12 +0200

    Helper and Utility

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,15 +1,20 @@
 import React, { Component, Fragment } from 'react';
+import { getFunName } from '../helpers';
 
 class StorePicker extends Component {
-  render() {
-    return (
-      <form className="store-selector">
-        <h2>Inserire il Nome dello Store</h2>
-        <input type="text" required placeholder="Nome Store" />
-        <button type="submit">Visita lo Store -></button>
-      </form>
-    );
-  }
+	render() {
+		return (
+			<form className="store-selector">
+				<h2>Inserire il Nome dello Store</h2>
+				<input
+					type="text"
+					required
+					placeholder="Nome Store"
+					defaultValue={getFunName()} />
+				<button type="submit">Visita lo Store -></button>
+			</form>
+		);
+	}
 }
 
 export default StorePicker;
