80352baa23508c769cdedf140144b295da53276a
Simone Taeggi <simone.taeggi@leovegas.com>
2018-03-28 18:06:12 +0200

    Events, Refs and this Binding

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,13 +1,29 @@
-import React, { Component, Fragment } from 'react';
+import React from 'react';
 import { getFunName } from '../helpers';
 
-class StorePicker extends Component {
+class StorePicker extends React.Component {
+	constructor() {
+		super();
+		this.goToStore = this.goToStore.bind(this);
+	}
+	myInput = React.createRef();
+
+	goToStore(event) {
+		// 1. stoppa il submit della Form
+		event.preventDefault();
+		// 2. recupero il testo passato in input
+		console.log(this.myInput);
+		// 3. cambia la pagina mandandola a /store/testo-inserito
+
+
+	}
 	render() {
 		return (
-			<form className="store-selector">
+			<form className="store-selector" onSubmit={this.goToStore}>
 				<h2>Inserire il Nome dello Store</h2>
 				<input
 					type="text"
+					ref={this.myInput}
 					required
 					placeholder="Nome Store"
 					defaultValue={getFunName()} />
